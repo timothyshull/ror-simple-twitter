9ac6698e3d0159f5b5ef6d75a35c04c712718676
timothyshull <timothyshull@gmail.com>
2015-03-14 11:58:33 -0700

    Partial on Rails flavored Ruby

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,34 +3,34 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionController::TestCase
   
   def setup
-    @title_helper = " | Ruby on Rails Tutorial Sample App"
+    @base_title = "Ruby on Rails Tutorial Sample App"
   end
   
   def teardown
-    @title_helper = nil
+    @base_title = nil
   end
   
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home#{@title_helper}"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help#{@title_helper}"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About#{@title_helper}"
+    assert_select "title", "About | #{@base_title}"
   end
   
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact#{@title_helper}"
+    assert_select "title", "Contact | #{@base_title}"
   end
 end
